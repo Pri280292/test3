8f81ab9d6ce3ce3730dcc6b982376fab56af17fb
Priyanka Singh <p.singh.28021992@gmail.com>
2022-08-01 15:25:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StudentDataService } from './services/student-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test3';
+  students:any;
+  constructor(private studentData:StudentDataService){
+    this.studentData.studentsInfo().subscribe((data)=>{
+      this.students=data;
+     });
+  };
+  
+  saveStudentData(data :any){
+      console.warn(data);
+       this.studentData.saveStudentinfo(data).subscribe((result)=>{
+           console.warn(result);
+       })
+  }
+  status:any;
+  deleteStudentData(student:any){
+    
+    this.studentData.deleteStudentInfo(student.id).subscribe(result =>{
+       this.students=this.students.filter((item:any)=>item.id!==student.id)
+  
+    })
+
+  }
+  
+ 
+    
+
 }
